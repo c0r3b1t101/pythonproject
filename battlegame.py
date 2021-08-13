9c8d759c63f9ffe60c744c1b2ea5bc69839c777c
c0r3b1t101 <78095994+c0r3b1t101@users.noreply.github.com>
2021-08-12 22:29:44 -0500

    added sleep import

--- battlegame.py
+++ battlegame.py
@@ -1,3 +1,4 @@
+from time import sleep
 
 wizard = "Wizard"
 wizard_hp = 70
@@ -23,7 +24,7 @@ print("1)   Wizard")
 print("2)   Elf")
 print("3)   Human")
 print("4)   Orc")
-print("5) Exit The Game!")
+print("5)   Exit The Game!")
 character_select = input("Choose Your Character: ").lower()
 if character_select == "1" or character_select == "wizard":
     character_select = wizard
@@ -58,16 +59,20 @@ elif character_select == "5" or character_select == "exit":
 else:
     print("Unknown Character!")
 
+
 while True and character_select != "5" and character_select != "exit":
     print(f"The {character_select} damaged the dragon")
     dragon_hp = dragon_hp - my_dmg
     print(f"The Dragon's hitpoints are now: {dragon_hp}")
+    sleep(2.5)
     if dragon_hp <= 0:
         print("The Dragon has lost the battle!")
         break
     print(f"The dragon damaged the  {character_select}")
+    sleep(2.5)
     my_hp = my_hp - dragon_dmg
     print(f"My hitpoints are now: {my_hp}")
+    sleep(2.5)
     if my_hp <= 0:
         print(f"The {character_select} has lost the battle")
         break
